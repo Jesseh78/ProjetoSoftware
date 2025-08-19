0b52a54ed8fb232c6c70bd8aed09aa233edfb1ba
Jesseh78 <joseulises59@gmail.com>
2025-08-19 10:08:23 -0300

    aula 03

--- Aula03/Biblioteca/src/Main.java
+++ Aula03/Biblioteca/src/Main.java
@@ -5,9 +5,12 @@ import entidades.Livro;
 public class Main {
     public static void main(String[] args) {
         Livro l1 = new Livro("Harry Potter", "sei la o autor", 500 , 560.0);
-        System.out.println(l1.toString());
 
-        Livro l2 = new Livro("livro 2 ", "sei la");
-        System.out.println(l2.toString());
+
+        l1.emprestarLivro(100);
+        l1.devolverLivro(1);
+        System.out.println(l1.toString());
+        //Livro l2 = new Livro("livro 2 ", "sei la");
+        //System.out.println(l2.toString());
     }
 }
---
+++ Aula03/Biblioteca/src/entidades/Livro.java
@@ -0,0 +1,49 @@
+package entidades;
+
+public class Livro {
+    public String titulo;
+    public String autor;
+    public int quantidade = 0;
+    public double valor = 0;
+
+    public Livro(String titulo, String autor, int quantidade, double valor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    public Livro(String autor, String titulo) {
+        this.autor = autor;
+        this.titulo = titulo;
+    }
+    public void emprestarLivro(int quantidade) {
+        if (quantidade > 0 && quantidade <= this.quantidade) {
+            this.quantidade -= quantidade;
+        }else{
+            System.out.println("Valor invalido");
+        }
+    }
+    public void devolverLivro(int quantidade) {
+        if (quantidade > 0 && quantidade <= this.quantidade){
+            this.quantidade += quantidade;
+        }else if(quantidade > this.quantidade){
+            System.out.println("Devolveu livros a mais");
+        }else{
+            System.out.println("Valor invalido");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "------------ " +
+                "\ntitulo: " + this.titulo +
+                "\nautor: " + this.autor +
+                "\nquantidade: " + this.quantidade +
+                "\nvalor: R$" + this.valor+
+                "\n----------";
+    }
+    public int emprestarLivro() {
+        return 0;
+    }
+}
