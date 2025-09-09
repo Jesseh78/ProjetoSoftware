721e5178afc152350b2cdf13d3a0b4ebbeac9d80
Jesseh78 <joseulises59@gmail.com>
2025-09-09 10:43:09 -0300

    alteração do exercicio aula 06

--- Aula06/SistemaBancario/src/Main.java
+++ Aula06/SistemaBancario/src/Main.java
@@ -3,6 +3,8 @@ import entidades.Cartao;
 import entidades.Pagamento;
 import entidades.Pix;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -10,60 +12,80 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o seu nome: ");
-        String nomeCliente = sc.nextLine();
 
-        System.out.println("Digite a data de pagamento: ");
-        String dataPagamento = sc.nextLine();
+        List<Pagamento> pagamentos = new ArrayList<Pagamento>();
 
-        System.out.println("Digite o valor de pagamento: ");
-        double valorPagamento = sc.nextDouble();
-        sc.nextLine();
-        int opcoes;
+        while(true) {
+            // menu de opções
+            System.out.println("1 - Cadastrar pagamento");
+            System.out.println("2 - Listar pagamentos");
+            System.out.println("3 - Sair");
+            String opcao = sc.nextLine();
 
-            System.out.println("Informe método de pagamento");
-            System.out.println("1 - boleto");
-            System.out.println("2 - PIX");
-            System.out.println("3 - Cartão");
-            System.out.println("4 - exibir");
-            System.out.println("0 - Sair");
-            System.out.print("Escolha uma opção: ");
+            if (opcao.equals("1")) {
+                System.out.println("Digite o seu nome: ");
+                String nomeCliente = sc.nextLine();
 
-            Pagamento pagamento = null;
-            int opcao = sc.nextInt();
-            sc.nextLine();
+                System.out.println("Digite a data de pagamento: ");
+                String dataPagamento = sc.nextLine();
 
-            switch(opcao){
-                case 1:
-                    System.out.println("Digite o código de barras: ");
-                    String codigoBarras = sc.nextLine();
-                    sc.nextLine();
-                    pagamento = new Boleto(nomeCliente, dataPagamento, valorPagamento, codigoBarras);
-                    System.out.println(pagamento.toString());
-                    break;
-                case 2:
-                    System.out.println("Digite a chave pix: ");
-                    String chavePix = sc.nextLine();
-                    sc.nextLine();
-                    pagamento = new Pix(nomeCliente, dataPagamento, valorPagamento, chavePix);
-                    System.out.println(pagamento.toString());
-                    break;
-                case 3:
-                    System.out.println("Digite o numero do cartão: ");
-                    int nCartao = sc.nextInt();
-                    sc.nextLine();
-                    pagamento = new Cartao(nomeCliente, dataPagamento, valorPagamento, nCartao);
-                    System.out.println(pagamento.toString());
-                    break;
-                case 0:
-                    System.out.println("Fechando...");
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
+                System.out.println("Digite o valor de pagamento: ");
+                double valorPagamento = sc.nextDouble();
+                sc.nextLine();
+
+                // menu de métodos de pagamentos
+                Pagamento pagamento = null;
+                int metodos = sc.nextInt();
+                sc.nextLine();
+
+                System.out.println("Informe método de pagamento");
+                System.out.println("1 - boleto");
+                System.out.println("2 - PIX");
+                System.out.println("3 - Cartão");
+                System.out.println("0 - Sair");
+                System.out.print("Escolha uma opção: ");
+
+                switch (metodos) {
+                    case 1:
+                        System.out.println("Digite o código de barras: ");
+                        String codigoBarras = sc.nextLine();
+                        sc.nextLine();
+                        pagamento = new Boleto(nomeCliente, dataPagamento, valorPagamento, codigoBarras);
+                        System.out.println("Boleto cadastrado com sucesso!");
+                        break;
+                    case 2:
+                        System.out.println("Digite a chave pix: ");
+                        String chavePix = sc.nextLine();
+                        sc.nextLine();
+                        pagamento = new Pix(nomeCliente, dataPagamento, valorPagamento, chavePix);
+                        System.out.println("Pix cadastrado com sucesso!");
+                        break;
+                    case 3:
+                        System.out.println("Digite o numero do cartão: ");
+                        int nCartao = sc.nextInt();
+                        sc.nextLine();
+                        pagamento = new Cartao(nomeCliente, dataPagamento, valorPagamento, nCartao);
+                        System.out.println("Cartao cadastrado com sucesso!");
+                        break;
+                    case 0:
+                        System.out.println("Fechando...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                        continue;
+                }
+                pagamentos.add(pagamento);
             }
-
-
-
-        sc.close();
+            else if (opcao.equals("2")) {
+                System.out.println("Lista de pagamentos: ");
+                for (Pagamento pagamento : pagamentos) {
+                    System.out.println(pagamento.toString());
+                }
+                System.out.println("---------------------");
+            }else{
+                System.out.println("Finalizando processo...");
+            }
+            sc.close();
+        }
     }
 }
